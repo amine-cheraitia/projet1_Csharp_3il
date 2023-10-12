aa5d0fe60d5fbe7fe5f1d971329c3c7ec2f5c8b5
Amine Cheraitia <amine-cheraitia@hotmail.com>
2023-10-12 09:33:16 +0200

    clear la console

--- ProjectsStd/Program.cs
+++ ProjectsStd/Program.cs
@@ -21,12 +21,14 @@ foreach (var etudiant in listeEtudiants)
 {
     Console.WriteLine($"Est-ce que {etudiant} est présent(e) ? O/N");
     var reponse = Console.ReadLine().ToLower();
+    Console.Clear();
     // en cas d'erreur de saisie on demande a l'utilisateur de choisir O ou N
     while (reponse != "n" && reponse != "o")
     {
         Console.WriteLine("La réponse est incorrecte, veuillez choisir O ou N comme réponse.");
         Console.WriteLine($"Est-ce que {etudiant} est présent(e) ? O/N");
         reponse = Console.ReadLine().ToLower();
+        Console.Clear();
     }
     // si l'utilisateur saisie n on rajoute l'étudiant concerné a listeEtudiantsAbsents
     if (reponse == "n")
@@ -53,3 +55,5 @@ Console.WriteLine(@"
 /\______\/\______\/\______\/\______\
 \/______/\/______/\/______/\/______/                                                                                                                                                      
 ");
+
+Console.ReadLine();
